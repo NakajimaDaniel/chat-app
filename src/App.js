00e71197557a23a122db2ceee402d4b4ec3bbcc7
NakajimaDaniel <danieln17.contato@outlook.com>
2021-01-08 17:12:43 -0300

    chat css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import react, {useState, useEffect} from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
 
 function App() {
+
+  const [value, setValue] = useState();
+
+  const [messageList, setMessageList] = useState([]);
+
+
+  const onChangeInput = (e)=>{
+    setValue(e.target.value)
+  }
+
+  const onClickButton = ()=>{
+    if(value != "")
+    {
+      setMessageList([...messageList, {'message':value}])
+      setValue("");
+    }
+
+  }
+
+  const handleKeyPress = (e)=>{
+    if(e.key==='Enter'){
+      onClickButton();
+    }
+  }
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="menu-wrap">
+        
+      </div>
+
+      <div className="chat-main">
+        <div className="message-list">
+        {
+          messageList.map((val)=>{
+            return(
+
+              <div className="message">{val.message}</div>
+
+            );
+          })
+        }
+        </div>
+        <div className="message-input">
+          <textarea cols='2' onChange={onChangeInput} onKeyPress={handleKeyPress} value={value}></textarea>
+          <button onClick={onClickButton}>submit</button>
+        </div>
+      </div>
+
+
+
+ 
+
+
+      
     </div>
   );
 }
